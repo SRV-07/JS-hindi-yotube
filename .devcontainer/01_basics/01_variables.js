2f5b1b6a98ca5b860a341320f67997fe82f5c0b4
Sourav Kumar <149148414+SRV-07@users.noreply.github.com>
2023-10-28 12:18:38 +0000

    variable commit done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "sourav@google.com"
+var accountPassword = "12345"
+accountCity = "ranchi"
+
+//accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
