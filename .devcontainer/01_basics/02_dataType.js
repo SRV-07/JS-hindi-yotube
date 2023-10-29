0570fca918393d545d2092869c9c144c8bd59c68
Sourav Kumar <149148414+SRV-07@users.noreply.github.com>
2023-10-29 19:15:35 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,18 @@
+console.log(3 + 3);
+console.log ("sourav")
+
+
+let name = "sourav"
+let age = 25
+let isLoggedIn = false
+
+// number = 2 to power 53
+// bigint 
+// string = ""
+// boolean = true/false
+// null = true/false 
+//  undefined 
+// symbol
+
+// how to know typeof
+console.log(typeof null);
