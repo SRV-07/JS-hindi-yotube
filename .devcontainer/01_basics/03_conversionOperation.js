e2df05fa140281d15ef12f2b8250fb9fc2b7d088
Sourav Kumar <149148414+SRV-07@users.noreply.github.com>
2023-10-30 08:34:09 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+ let score = "sourav"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" = 33
+// "3abc" = NaN
+// TRUE = 1; false = 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
